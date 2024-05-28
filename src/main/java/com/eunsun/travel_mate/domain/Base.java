2ad74bba0be5271016ae6ad37ef5ad935e0cfbb1
eunsun <eunsunjj9501@gmail.com>
2024-05-28 20:10:47 +0900

    refactor: createdAt, updatedAt format change - createdAt, updatedAt 포맷 변경

--- src/main/java/com/eunsun/travel_mate/domain/Base.java
+++ src/main/java/com/eunsun/travel_mate/domain/Base.java
@@ -1,5 +1,6 @@
 package com.eunsun.travel_mate.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -17,8 +18,10 @@ public abstract class Base {
 
   @CreatedDate
   @Column(updatable = false)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime createdAt;
 
   @LastModifiedDate
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime updatedAt;
 }
