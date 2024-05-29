79a452d2d2df61c8333754a8008686ad534cf5ed
eunsun <eunsunjj9501@gmail.com>
2024-05-29 14:47:24 +0900

    test: Write UserRepository testCode - UserRepository existsByEmail() 테스트 - @AfterEach 사용으로 각 테스트 메서드 실행 후 데이터 정리

---
+++ src/test/java/com/eunsun/travel_mate/repository/UserRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.eunsun.travel_mate.repository;
+
+import com.eunsun.travel_mate.domain.User;
+import java.time.LocalDate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserRepositoryTest {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @AfterEach
+  void cleanUp() {
+    userRepository.deleteAll();
+  }
+
+  @Test
+  @DisplayName("이메일이 DB에 존재하는 경우")
+  void existsByEmail() {
+
+    // given
+    User user = User.builder()
+        .email("text@email.com")
+        .birthdate(LocalDate.of(2000, 11, 22))
+        .password("ABCd123@")
+        .name("test")
+        .build();
+    userRepository.save(user);
+
+    // when
+    boolean exist = userRepository.existsByEmail("text@email.com");
+
+    // then
+    Assertions.assertThat(exist).isTrue();
+  }
+
+  @Test
+  @DisplayName("이메일이 DB에 존재하지 않는 경우")
+  void noneExistsByEmail() {
+
+    // given
+
+    // when
+    boolean exist = userRepository.existsByEmail("text@email.com");
+
+    // then
+    Assertions.assertThat(exist).isFalse();
+  }
+}
