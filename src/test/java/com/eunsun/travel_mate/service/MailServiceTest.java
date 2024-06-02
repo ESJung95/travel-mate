2c43ff94e7480717281dd689ee8e7bfbd6ba299b
eunsun <eunsunjj9501@gmail.com>
2024-06-03 03:41:32 +0900

    test: Add mail sending test code - 회원가입 기능 구현 시 놓친 test 추가

--- src/test/java/com/eunsun/travel_mate/service/MailServiceTest.java
+++ src/test/java/com/eunsun/travel_mate/service/MailServiceTest.java
@@ -1,5 +1,10 @@
 package com.eunsun.travel_mate.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,11 +13,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class MailServiceTest {
