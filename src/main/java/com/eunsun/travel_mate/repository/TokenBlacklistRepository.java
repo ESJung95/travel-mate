6d486250743713912ef86a05e2414c6c30327b56
eunsun <eunsunjj9501@gmail.com>
2024-06-03 17:31:45 +0900

    feat: Automatically clean up expired tokens - 스케줄링 기능으로 블랙리스트에 저장된 유효기간 만료된 토큰을 자동으로 삭제 - 토큰의 유효기간을 1분으로 변경하고, 삭제 주기를 2분으로 설정

--- src/main/java/com/eunsun/travel_mate/repository/TokenBlacklistRepository.java
+++ src/main/java/com/eunsun/travel_mate/repository/TokenBlacklistRepository.java
@@ -1,6 +1,8 @@
 package com.eunsun.travel_mate.repository;
 
 import com.eunsun.travel_mate.domain.TokenBlacklist;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 public interface TokenBlacklistRepository extends JpaRepository<TokenBlacklist, Long> {
 
   boolean existsByToken(String token);
+
+  List<TokenBlacklist> findAllByExpiredTimeBefore(LocalDateTime dateTime);
 }
--- src/main/java/com/eunsun/travel_mate/service/TokenBlacklistService.java
+++ src/main/java/com/eunsun/travel_mate/service/TokenBlacklistService.java
@@ -3,8 +3,11 @@ package com.eunsun.travel_mate.service;
 import com.eunsun.travel_mate.domain.TokenBlacklist;
 import com.eunsun.travel_mate.dto.request.TokenBlacklistRequestDto;
 import com.eunsun.travel_mate.repository.TokenBlacklistRepository;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -32,5 +35,22 @@ public class TokenBlacklistService {
   // 토큰이 블랙리스트에 존재하는지 확인
   public boolean isTokenBlacklisted(String token) {
     return tokenBlacklistRepository.existsByToken(token);
+  }
+
+  // 유효 시간이 지난 토큰 삭제 - 스케줄링 작업
+  @Scheduled(fixedRate = 120000) // 1시간마다 실행 = 3600000
+  public void cleanUpExpiredTokens() {
+    LocalDateTime now = LocalDateTime.now();
+    List<TokenBlacklist> expiredTokens = tokenBlacklistRepository.findAllByExpiredTimeBefore(now);
+
+    if (!expiredTokens.isEmpty()) {
+      tokenBlacklistRepository.deleteAll(expiredTokens);
+      log.info("만료된 토큰이 블랙리스트에서 삭제되었습니다.");
+
+      // 만료된 토큰들을 로그로 출력
+      for (TokenBlacklist token : expiredTokens) {
+        log.info("Expired Token: {}", token.getToken());
+      }
+    }
   }
 }
