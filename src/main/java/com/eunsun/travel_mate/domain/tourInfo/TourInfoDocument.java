341945b11cf4cd248688919629e361d007d06546
eunsun <eunsunjj9501@gmail.com>
2024-06-12 01:22:05 +0900

    refactor: delete and recreate ClassName - TourInfoDocument 재생성

--- src/main/java/com/eunsun/travel_mate/domain/tourInfo/TourInfoDocument.java
+++ src/main/java/com/eunsun/travel_mate/domain/tourInfo/TourInfoDocument.java
@@ -1,9 +1,13 @@
 package com.eunsun.travel_mate.domain.tourInfo;
 
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
@@ -12,10 +16,15 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TourInfoDocument {
 
   @Id
-  private String id;
+  private Long id;
+
+  @Field(type = FieldType.Keyword)
+  private String areaCodeName;
 
   @Field(type = FieldType.Keyword)
   private String areaCode;
@@ -50,10 +59,37 @@ public class TourInfoDocument {
   @Field(type = FieldType.Text)
   private String imageUrl2;
 
-  @Field(type = FieldType.Date)
+  @Field(type = FieldType.Text)
   private String createdTime;
 
-  @Field(type = FieldType.Date)
+  @Field(type = FieldType.Text)
   private String modifiedTime;
 
+  @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+  private LocalDateTime createdAt;
+
+  @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+  private LocalDateTime updatedAt;
+
+  public static TourInfoDocument from(TourInfo tourInfo) {
+    return TourInfoDocument.builder()
+        .id(tourInfo.getTourInfoId())
+        .areaCode(String.valueOf(tourInfo.getAreaCode()))
+        .areaCodeName(tourInfo.getAreaCode().getName())
+        .sigunguCode(String.valueOf(tourInfo.getSigunguCode()))
+        .title(String.valueOf(tourInfo.getTitle()))
+        .addr1(String.valueOf(tourInfo.getAddr1()))
+        .addr2(String.valueOf(tourInfo.getAddr2()))
+        .contentTypeId(String.valueOf(tourInfo.getContentTypeId()))
+        .contentId(String.valueOf(tourInfo.getContentId()))
+        .mapx(Double.valueOf(tourInfo.getMapx()))
+        .mapy(Double.valueOf(tourInfo.getMapy()))
+        .imageUrl1(String.valueOf(tourInfo.getImageUrl1()))
+        .imageUrl2(String.valueOf(tourInfo.getImageUrl2()))
+        .createdTime(String.valueOf(tourInfo.getCreatedTime()))
+        .modifiedTime(String.valueOf(tourInfo.getModifiedTime()))
+        .createdAt(tourInfo.getCreatedAt())
+        .updatedAt(tourInfo.getUpdatedAt())
+        .build();
+  }
 }
