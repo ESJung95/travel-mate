113664f6cb4de80d7554a93bbb7f62e26c223efb
eunsun <eunsunjj9501@gmail.com>
2024-06-14 02:25:22 +0900

    refactor: Add annotation - Setter 어노테이션 추가

--- src/main/java/com/eunsun/travel_mate/dto/request/UpdatePlanRequestDto.java
+++ src/main/java/com/eunsun/travel_mate/dto/request/UpdatePlanRequestDto.java
@@ -3,8 +3,10 @@ package com.eunsun.travel_mate.dto.request;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class UpdatePlanRequestDto {
 
