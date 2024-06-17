b804d5e743e43c43cacf86b6a897c22038b250c5
eunsun <eunsunjj9501@gmail.com>
2024-06-17 18:54:01 +0900

    refactor: Add description - swagger 기능 설명 추가

--- src/main/java/com/eunsun/travel_mate/config/SwaggerConfig.java
+++ src/main/java/com/eunsun/travel_mate/config/SwaggerConfig.java
@@ -14,8 +14,9 @@ public class SwaggerConfig {
   public OpenAPI openAPI() {
     return new OpenAPI()
         .info(new Info()
-            .title("Travel Mate")
-            .description("")
+            .title("Travel Mate - 여행 친구")
+            .description("누구나 쉽게 여행 정보를 얻을 수 있도록 도와주고,\n"
+                + "일정표를 작성하는 기능을 제공하는 서비스입니다.")
             .version("1.0")
         );
   }
