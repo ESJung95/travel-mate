d3a89828550e8d19a280d446b304a8709bd05f0c
eunsun <eunsunjj9501@gmail.com>
2024-06-17 21:20:55 +0900

    docs: Add Swagger documentation for AreaCodeController - 엔드포인트에 @Operation, @ApiResponse 추가

--- src/main/java/com/eunsun/travel_mate/controller/AreaCodeController.java
+++ src/main/java/com/eunsun/travel_mate/controller/AreaCodeController.java
@@ -2,6 +2,9 @@ package com.eunsun.travel_mate.controller;
 
 import com.eunsun.travel_mate.domain.AreaCode;
 import com.eunsun.travel_mate.service.AreaCodeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,12 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/area-code")
+@Tag(name = "AreaCode", description = "지역 코드 API")
+
 public class AreaCodeController {
 
   private final AreaCodeService areaCodeService;
 
   // 지역 코드 전체 조회
   @GetMapping
+  @Operation(
+      summary = "OpenApi에서 전체 정보 가져오기",
+      description = "OpenApi에서 지역 코드 전체를 가져와 저장합니다.")
+  @ApiResponse(responseCode = "200", description = "성공")
   public ResponseEntity<?> getAreaCodeFromApi() {
     List<AreaCode> areaCodes =  areaCodeService.getAreaCodeFromApi();
     return ResponseEntity.ok(areaCodes);
