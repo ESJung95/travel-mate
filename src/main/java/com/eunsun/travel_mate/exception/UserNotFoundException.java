377497aca2651c20227ec017cfdbacfb0c538f7f
eunsun <eunsunjj9501@gmail.com>
2024-06-20 20:38:50 +0900

    refactor: Modify UserNotFoundException - 사용자를 찾을 수 없는 예외 코드 수정

--- src/main/java/com/eunsun/travel_mate/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.eunsun.travel_mate.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-  public UserNotFoundException(String message) {
-    super(message);
-  }
-}
